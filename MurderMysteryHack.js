fa260bf3b27cbdbd796d1bb7386b6730edbcd0d1
tk400 <takakoti400@protonmail.com>
2022-12-26 18:26:34 +0900

    Added Copy Option and failsafe
    and some screen fix.

--- MurderMysteryHack.js
+++ MurderMysteryHack.js
@@ -7,6 +7,8 @@ script.import("lib/arrayFunctions.js")
 
 script.import("tKore.js")
 
+var GuiScreen = Java.type('net.minecraft.client.gui.GuiScreen');
+
 var Swords = Java.type('net.minecraft.item.ItemSword');
 var Spade = Java.type('net.minecraft.item.ItemSpade');
 var ItemMap = Java.type('net.minecraft.item.ItemMap');
@@ -19,11 +21,13 @@ function MurdererHack() {
    var Murderername=SaidPlayers=[]
    var Murders=0;
    var TellMurderer = value.createBoolean("TellMurderer", false);
+   var CopyMurderer = value.createBoolean("CopyMurderer", false);
    var sayrange = value.createFloat("MaxSpeakRange", 6, 0, 30);
    var MurdersVal = value.createInteger("MaxMurderers", 1,1,2);
  
    this.addValues = function(v) {
       v.add(TellMurderer)
+      v.add(CopyMurderer)
       v.add(sayrange)
       v.add(MurdersVal)
    }
@@ -52,16 +56,16 @@ function MurdererHack() {
                   }
                   if(!([30,46,50,95,148,262,266,332,339,355,397,402,416].includes(Item.getIdFromItem(helditem)) || checkhelditem(ItemMap) || checkhelditem(ItemBow) || checkhelditem(ItemPotion) || (checkhelditem(Spade) ? (helditem.getToolMaterial()=="WOOD" ? true : false) : false)) || checkhelditem(Swords)) {
                      var entname=entities.getName()
-                     if(!Murderername.includes(entname)) {
+                     if(!Murderername.includes(entname) && (Murders < MurdersVal.get())) {
                         chat.print(entname+ " has equiped an Item!")
                         chat.print("§2ItemDetail §r[§e"+helditem+ +"§r] §3Id§r :[§d"+Item.getIdFromItem(helditem)+"§r] "+"("+Item.getByNameOrId(Item.getIdFromItem(helditem))+")")
                         chat.print("§5Detected Murderer §r: §4" +entname)
+                        if(CopyMurderer.get()) {GuiScreen.setClipboardString(entname);chat.print(entname + " has copied to clipboard.")}//hm mc.keyboard.setClipboard is not worked, anyway new alter code has worked... good good
                         Murderername.push(entname)
                         Murders++
                      }
-                     if(TellMurderer.get() && !SaidPlayers.includes(entname) && mc.thePlayer.getDistanceToEntity(entities) <= sayrange.get()) {
-                        sendChat(entname +" is Murderer!");
-                        SaidPlayers.push(entname);
+                     if(TellMurderer.get() && !SaidPlayers.includes(entname) && mc.thePlayer.getDistanceToEntity(entities) <= sayrange.get() && SaidPlayers.length < MurdersVal.get()) {
+                        if(mc.thePlayer.ticksExisted >= 100) {sendChat(entname +" is Murderer!");SaidPlayers.push(entname)}else{chat.print("FailSafe triggerd, now disabling..."); moduleManager.getModule(this.getName()).state = false}
                      }
                   }
                }
@@ -73,10 +77,11 @@ function MurdererHack() {
       Murders=0;SaidPlayers=[];Murderername=[];
    }
 	this.onRender2D = function(){
+      //mc.ingameGUI.drawString(mc.fontRendererObj, "Debug ticks:"+mc.thePlayer.ticksExisted, mc.displayWidth / 4, mc.displayHeight / 2.5 - 10, -1)
 		if(Murderername.length>=1) {
-         mc.ingameGUI.drawString(mc.fontRendererObj, "§lMurderer§r:"+Murderername.toString(), mc.displayWidth / 2, mc.displayHeight / 2.5, -1)
+         mc.ingameGUI.drawString(mc.fontRendererObj, "§lMurderer§r:"+Murderername.toString(), mc.displayWidth / 4, mc.displayHeight / 2.5, -1)
 		} else {
-			mc.ingameGUI.drawString(mc.fontRendererObj, "§lMurderer: §r§4" + "No murderer found", mc.displayWidth / 2, (mc.displayHeight / 2.5), -1);
+			mc.ingameGUI.drawString(mc.fontRendererObj, "§lMurderer: §r§4" + "No murderer found", mc.displayWidth / 4, (mc.displayHeight / 2.5), -1);
 		}
 	}
    this.onEnable = function() {}
